73508b6cc1009b7ed2d70d2c65a0853bd5dfd61e
dan <dan@pop-os.localdomain>
2019-10-09 21:17:11 +0700

    xong cac api con lai

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ApiNckh  
 - API để lưu file cho phép đẩy file lên, trả về text và ID, thêm mức độ để xử lý file đẩy lên, (deskew, deblur, autofill, xử lý bảng,(lưu file và text cùng thư mục temp)
 - [x] API nhận text và ID, đẩy lên elastic search.
+api có dạng: localhost:8080//pushtextandid  
 json client gửi lên có định dạng:  
   {  
   &nbsp;&nbsp;&nbsp;"mac":"123456sodp",  
@@ -8,6 +9,7 @@ json client gửi lên có định dạng:
   &nbsp;&nbsp;&nbsp;"contents":"noi dung cua file"  
   }  
 - [x] API gửi lên đoạn text, trả về danh sách các tên file, nội dung của file được giới hạn 100 từ và ID đi kèm
+api có dạng: localhost:8080//search    
 json client gửi lên có dạng:  
 {  
 	&nbsp;&nbsp;&nbsp;"mac":"documents",  
@@ -31,9 +33,21 @@ json trả về có dạng mảng:
     }
 ]  
 - API nhận ID, chỉ trả về file text
-api: localhost:8080/getAllContents
-json client tải lên có dạng:  
+api: localhost:8080/getAllContents  
+json client tải lên có dạng:    
 {  
 	"id":"1"  
 }  
-- API Download sẽ tải file gốc về
+- API Download nhận id từ client và trả file gốc về
++ trước tiên phải gọi tới Api con trả về extension của file gốc
+api có dạng: localhost:8080/getRootFileExtension  
+json client tải lên có dạng:    
+{  
+	"id":"1"  
+}
++ sau đó sẽ gọi tới Api download file gốc về  
+api có dạng: localhost:8080/download  
+json client tải lên có dạng:    
+{  
+	"id":"1"  
+}  
--- apiTextSearch/apiTextSearch.go
+++ apiTextSearch/apiTextSearch.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -178,7 +180,98 @@ func getAllContents(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func downloadFile(writer http.ResponseWriter, r *http.Request) {
+	var newEvent idstruct
+	var fileName string
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(writer, "data is not enable")
+	} else {
+		json.Unmarshal(reqBody, &newEvent)
+		writer.WriteHeader(http.StatusCreated)
+		if exists("./saved/" + newEvent.ID) {
+			listFile := returnAllFileName("./saved/" + newEvent.ID)
+			for _, value := range listFile {
+				if len(listFile) > 2 {
+					if !strings.Contains(value, "txt") && strings.Contains(value, ".") {
+						fileName = value
+						break
+					}
+				} else {
+					if strings.Contains(value, ".") {
+						fileName = value
+						break
+					}
+				}
+
+			}
+		}
+	}
+	Filename := "./saved/" + newEvent.ID + "/" + fileName
+	//Check if file exists and open
+	Openfile, err := os.Open(Filename)
+	defer Openfile.Close() //Close after function return
+	if err != nil {
+		//File not found, send 404
+		http.Error(writer, "File not found.", 404)
+		return
+	}
+
+	//File is found, create and send the correct headers
+
+	//Get the Content-Type of the file
+	//Create a buffer to store the header of the file in
+	FileHeader := make([]byte, 512)
+	//Copy the headers into the FileHeader buffer
+	Openfile.Read(FileHeader)
+	//Get content type of file
+	FileContentType := http.DetectContentType(FileHeader)
+
+	//Get the file size
+	FileStat, _ := Openfile.Stat()                     //Get info from file
+	FileSize := strconv.FormatInt(FileStat.Size(), 10) //Get file size as a string
+
+	//Send the headers
+	writer.Header().Set("Content-Disposition", "attachment; filename="+Filename)
+	writer.Header().Set("Content-Type", FileContentType)
+	writer.Header().Set("Content-Length", FileSize)
+
+	//Send the file
+	//We read 512 bytes from the file already, so we reset the offset back to 0
+	Openfile.Seek(0, 0)
+	io.Copy(writer, Openfile) //'Copy' the file to the client
+	return
+}
+
+func getExt(writer http.ResponseWriter, r *http.Request) {
+	var newEvent idstruct
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(writer, "data is not enable")
+	} else {
+		json.Unmarshal(reqBody, &newEvent)
+		writer.WriteHeader(http.StatusCreated)
+		if exists("./saved/" + newEvent.ID) {
+			listFile := returnAllFileName("./saved/" + newEvent.ID)
+			for _, value := range listFile {
+				if len(listFile) > 2 {
+					if !strings.Contains(value, "txt") && strings.Contains(value, ".") {
+						fmt.Fprintf(writer, strings.Split(value, ".")[1])
+						break
+					}
+				} else {
+					if strings.Contains(value, ".") {
+						fmt.Fprintf(writer, strings.Split(value, ".")[1])
+						break
+					}
+				}
+			}
+		}
+	}
+}
 func setupRoutes() {
+	http.HandleFunc("/getRootFileExtension", getExt)
+	http.HandleFunc("/download", downloadFile)
 	http.HandleFunc("/getAllContents", getAllContents)
 	http.HandleFunc("/search", search)
 	http.HandleFunc("/pushtextandid", receiveContentAndID)
